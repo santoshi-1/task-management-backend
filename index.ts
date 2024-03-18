352dd209ecaffcdebf8f97a788c89da5880d38dc
santoshi-1 <karuturisantoshi@gmail.com>
2024-03-18 15:09:31 -0400

    data base config

--- index.ts
+++ index.ts
@@ -4,11 +4,22 @@ import express, {
   Response,
 } from 'express';
 import dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
 
 //To instantiate express app
 const app: Express = express();
 dotenv.config();
 
+//create Database connection
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true,
+});
 //Define server port
 const port = process.env.PORT;
 
@@ -17,5 +28,12 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-//start listening to the requests on the defined portapp
-app.listen(port);
+AppDataSource.initialize()
+  .then(() => {
+    //start listening to the requests on the defined portapp
+    app.listen(port);
+    console.log('Data source initialized');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
